497575e072b41e878ed76cfd4576feaadf978455
HectorJosueDH <hectorjosue78@gmail.com>
2023-08-08 11:16:33 -0600

    Modified index.js, now there's a class for each figure for running the tests

--- index.js
+++ index.js
@@ -1,74 +1,59 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const shapes = require('./lib/shapes.js');
-
-// SVG generator function that takes in the user input and returns the logo.svg
-function generateSVG(text, textColor, shape, shapeColor) {
-  let shapeEl;
-
-  // Determines the shape based on the user input
-  if (shape === 'circle') {
-    // Calls the generateCircle function from the shapes.js file
-    shapeEl = shapes.generateCircle(shapeColor);
-  } else if (shape === 'triangle') {
-    // Calls the generateTriangle function from the shapes.js file
-    shapeEl = shapes.generateTriangle(shapeColor);
-  } else if (shape === 'square') {
-    // Calls the generateSquare function from the shapes.js file
-    shapeEl = shapes.generateSquare(shapeColor);
-  } 
-
-  // SVG code generation with the chosen shape and dimensions
-  const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" viewBox="0 0 300 200">
-    ${shapeEl}
-    <text x="50%" y="50%" font-size="46" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${text}</text>
-    </svg>`;
-
-  return svg;
-}
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib");
 
 // Prompts the user for input using inquirer
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'text',
-      message: 'Enter text (up to three characters): ',
+      type: "input",
+      name: "text",
+      message: "Enter text (up to three characters): ",
       validate: (input) => input.length <= 3,
     },
     {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter text color keyword (OR a hexadecimal number): ',
+      type: "input",
+      name: "textColor",
+      message: "Enter text color keyword (OR a hexadecimal number): ",
     },
     {
-      type: 'list',
-      name: 'shape',
-      message: 'Choose a shape: ',
-      choices: ['circle', 'triangle', 'square'],
+      type: "list",
+      name: "shape",
+      message: "Choose a shape: ",
+      choices: ["circle", "triangle", "square"],
     },
     {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter shape color keyword (OR a hexadecimal number): ',
+      type: "input",
+      name: "shapeColor",
+      message: "Enter shape color keyword (OR a hexadecimal number): ",
     },
   ])
   .then((answers) => {
-    // Destructuring the answers object
     const { text, textColor, shape, shapeColor } = answers;
 
-    // Generates the SVG code
-    const svgCode = generateSVG(text, textColor, shape, shapeColor);
+    // Create the shape instance based on user input
+    let shapeInstance;
+    if (shape === "circle") {
+      shapeInstance = new Circle();
+    } else if (shape === "triangle") {
+      shapeInstance = new Triangle();
+    } else if (shape === "square") {
+      shapeInstance = new Square();
+    }
+
+    // Set the color and generate the SVG code
+    shapeInstance.setColor(shapeColor);
+    const svgCode = generateSVG(text, textColor, shapeInstance);
 
     // Write the SVG code to a file named 'logo.svg'
-    fs.writeFile('logo.svg', svgCode, (err) => {
+    fs.writeFile("logo.svg", svgCode, (err) => {
       if (err) {
-        console.log('Error at generating the .SVG file:', err);
+        console.log("Error at generating the .SVG file:", err);
       } else {
-        console.log('Success at generating the logo.svg file!');
+        console.log("Success at generating the logo.svg file!");
       }
     });
   })
   .catch((error) => {
-    console.log('An error occurred:', error);
+    console.log("An error occurred:", error);
   });
